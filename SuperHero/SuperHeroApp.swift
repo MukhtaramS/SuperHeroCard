c65e41ef721b9e7982facd777c13784e269d7c86
MukhtaramS <sulaimonovmukhtaram@gmail.com>
2025-11-26 19:33:11 +0100

    Initial Commit

---
+++ SuperHero/SuperHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp.swift
+//  SuperHero
+//
+//  Created by Mukhtaram Sulaimonov on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
